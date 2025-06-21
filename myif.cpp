19060919d9598b8f6179908838aa973f23830018
qoalscks <baeminchan10302@gmail.com>
2025-06-21 14:57:50 +0900

    Add files via upload

---
+++ myif.cpp
@@ -0,0 +1,548 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (apple == 15)
+//	{
+//		printf("apple은 15개 있습니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (apple != 17)
+//	{
+//		printf("apple은 17개가 아닙니다");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (apple > 7)
+//	{
+//		printf("apple은 7개보다 많습니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (apple < 20)
+//	{
+//		printf("apple은 20개보다 적습니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (apple)
+//	{
+//		printf("apple은 0개가 아닙니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (!apple)
+//	{
+//		printf("apple은 하나도 없습네다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 0;
+//	if (apple == 10)
+//	{
+//		printf("apple은 10개 있습니다.\n");
+//	}
+//	else if (apple == 0)
+//	{
+//		printf("apple은 하나도 없습니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int apple = 15;
+//	if (!apple)
+//	{
+//		printf("apple은 하나도 없습니다.\n");
+//	}
+//	else
+//	{
+//		printf("apple은 0개가 아닙니다");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int input;
+//  printf("숫자를 입력하세요 : ");
+//	scanf("%d", &input);
+//	if (input < 0)
+//	{
+//		printf("음수입니다");
+//	}
+//	else if (input > 0)
+//	{
+//		printf("양수입니다");
+//	}
+//	else
+//	{
+//		printf("0입니다");
+//	}
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int input;
+//	printf("숫자를 입력하세요 : ");
+//	scanf("%d", &input);
+//	if (input >= 10 && input <= 99)
+//	{
+//		printf("input은 두 자리 숫자입니다");
+//	}
+//	else
+//	{
+//		printf("input은 두 자리 숫자가 아닙니다");
+//	}
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int height = 170;
+//	if (height > 150)
+//	{
+//		printf("키가 150 이상입니다.\n");
+//	}
+//	if (height > 160)
+//	{
+//		printf("키가 160 이상입니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int height = 170;
+//	if (height > 150) {
+//		printf("키가 150 이상입니다");
+//	}
+//	else if (height > 160)
+//	{
+//		printf("키가 160 이상입니다");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int y_age;
+//	printf("나이를 입력하세요 : ");
+//	scanf("%d", &y_age);
+//	/*if (y_age > 0)
+//	{*/
+//		printf("전체 관람가\n");
+//	//}
+//	if (y_age >= 12)
+//	{
+//		printf("12세 관람가\n");
+//	}
+//	if (y_age >= 15)
+//	{
+//		printf("15세 관람가\n");
+//	}
+//	return 0;
+////}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char initial;
+//	scanf("%c", &initial); // A=65, a= 97
+//	if (initial >= 'A' && initial <= 92)
+//	{
+//		printf("대문자입니다");
+//	}
+//	else if (initial >= 97 && initial <= 124)
+//	{
+//		printf("소문자입니다");
+//	}
+//	else
+//	{
+//		printf("문자가 아닙니다");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다");
+//	}
+//	else
+//	{
+//		if (number < answer)
+//		{
+//			printf("숫자2가 숫자2보다 큽니다.\n");
+//		}
+//		else
+//		{
+//			printf("숫자1과 숫자2가 같습니다.\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다");
+//	}
+//	else
+//	{
+//		if (number < answer)
+//		{
+//			printf("숫자2가 숫자2보다 큽니다.\n");
+//		}
+//		else
+//		{
+//			printf("숫자1과 숫자2가 같습니다.\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다");
+//	}
+//	else
+//	{
+//		if (number < answer)
+//		{
+//			printf("숫자2가 숫자2보다 큽니다.\n");
+//		}
+//		else
+//		{
+//			printf("숫자1과 숫자2가 같습니다.\n");
+//		}
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer)
+//	{
+//		printf("숫자1이 숫자2보다 큽니다.\n");
+//	}
+//	else if (number < answer)
+//	{
+//		printf("숫자2가 숫자1보다 큽니다.\n");
+//	}
+//	else
+//	{
+//		printf("숫자1과 숫자2가 같습니다.\n")
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int score = 75;
+//	if (score > 90) {
+//		printf("성적은 A등급입니다.\n");
+//	}
+//	else if (score > 80) {
+//		printf("성적은 B등급입니다.\n");
+//	}
+//	else if (score > 70) {
+//		printf("성적은 C등급입니다.\n");
+//	}
+//	else if (score > 60) {
+//		printf("성적은 D등급입니다.\n");
+//	}
+//	else {
+//		printf("성적은 F등급입니다.\n");
+//	}
+//	return 0;
+////}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number = -11;
+//	if (number > 0) {
+//		printf("number는 양수입니다.\n");
+//	}
+//	else if (number < 0) {
+//		printf("number는 음수입니다.\n");
+//	}
+//	else {
+//		printf("number는 0입니다.\n");
+//	}
+//	if (number % 2 == 0)
+//	{
+//		printf("number는 짝수입니다.\n");
+//	}
+//	else
+//	{
+//		printf("number는 홀수입니다.\n");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char x;
+//	printf("w, a, d, s 중 하나를 입력하세요 : "); // a=97 w=120 d=100 s=116
+//	scanf("%c", &x);
+//	if (x == 'w')
+//	{
+//		printf("위 방향키를 입력하셨습니다.");
+//	}
+//	else if (x == 'a')
+//	{
+//		printf("좌 방향키를 입력하셨습니다.");
+//	}
+//	else if (x == 'd')
+//	{
+//		printf("우 방향키를 입력하셨습니다.");
+//	}
+//	else if (x == 's')
+//	{
+//		printf("아래 방향키를 입력하셨습니다.");
+//	}
+//	else
+//	{
+//		printf("잘못 입력하셨습니다");
+//	}
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int gauge;
+//	printf("0에서 100사이의 gauge를 입력하세요 : ");
+//	scanf("%d", &gauge);
+//	if (gauge >= 45 && gauge <= 55)
+//	{
+//		printf("perfect");
+//	}
+//	else if (gauge >= 35 && gauge <= 65)
+//	{
+//		printf("excellent");
+//	}
+//	else
+//	{
+//		printf("good");
+//	}
+//  return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int input;
+//	printf("수를 입력하시오 : ");
+//	scanf("%d", &input);
+//	if (input % 3 == 0)
+//	{
+//		printf("input은 3의 배수입니다.\n");
+//		if (input % 6 == 0)
+//		{
+//			printf("input은 6의 배수입니다.\n");
+//		}
+//		if (input % 9 == 0)
+//		{
+//			printf("input은 9의 배수입니다.\n");
+//		}
+//	}
+//	
+//	else
+//	{
+//		printf("input은 3, 6, 9 의 배수가 아닙니다");
+//	}
+//	
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int out;
+//	double money;
+//	scanf("%d %lf", &out, &money);
+//	if (money - out - 0.5 >= 0 && out % 5 == 0)
+//	{
+//		printf("%.2lf", money - out - 0.5);
+//	}
+//	else
+//	{
+//		printf("%.2lf", money);
+//	}
+//}
+
+#include <stdio.h>
+
+int main()
+{
+	int burger, drink, side, dessert, calorie=0;
+	//printf("버거 하나를 고르세요.\n1.치즈버거 (461 칼로리)\n2.생선버거 (431 칼로리)\n3.비건버거 (420 칼로리)\n4.없음\n");
+	scanf("%d", &burger);
+	//printf("음료 하나를 고르세요.\n1.탄산음료 (130 칼로리)\n2.오렌지 주스 (160 칼로리)\n3.우유 (118 칼로리)\n4.없음");
+	scanf("%d", &drink);
+	//printf("사이드 메뉴 하나를 고르세요.\n1.감자튀김(100 칼로리)\n2.구운감자 (57 칼로리)\n3.셰프 샐러드 (70 칼로리)\n4.없음\n");
+	scanf("%d", &side);
+	//printf("디저트를 고르세요.\n1.사과파이 (167 칼로리)\n2.순대 (226 칼로리)\n3.과일 컵 (75 칼로리)\n4.없음\n");
+	scanf("%d", &dessert);
+	if (burger == 1)
+	{
+		calorie += 461;
+	}
+	else if (burger == 2)
+	{
+		calorie += 431;
+	}
+	else if (burger == 3)
+	{
+		calorie += 420;
+	}
+	else if (burger == 4)
+	{
+		calorie +=0;
+	}
+	else
+	{
+		printf("잘못 입력하셨습니다.\n");
+	}
+	if (drink == 1)
+	{
+		calorie += 130;
+	}
+	else if (drink == 2)
+	{
+		calorie += 160;
+	}
+	else if (drink == 3)
+	{
+		calorie += 118;
+	}
+	else if (drink == 4)
+	{
+		calorie +=0;
+	}
+	else
+	{
+		printf("잘못 입력하셨습니다.\n");
+	}
+	if (side == 1)
+	{
+		calorie += 100;
+	}
+	else if (side == 2)
+	{
+		calorie += 57;
+	}
+	else if (side == 3)
+	{
+		calorie += 70;
+	}
+	else if (side == 4)
+	{
+		calorie += 0;
+	}
+	else
+	{
+		printf("잘못 입력하셨습니다.\n");
+	}
+	if (dessert == 1)
+	{
+		calorie += 167;
+	}
+	else if (dessert == 2)
+	{
+		calorie += 266;
+	}
+	else if (dessert == 3)
+	{
+		calorie += 75;
+	}
+	else if (dessert == 4)
+	{
+		calorie +=0;
+	}
+	else
+	{
+		printf("잘못 입력하셨습니다.\n");
+	}
+	printf("Your total Calorie count is %d.", calorie);
+	return 0;
+}
